4e82491ae79a63935f560271121e87354f4321d8
KRST23 <stefanno.clash@gmail.com>
2025-11-16 20:16:39 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -7,7 +7,7 @@ df = pd.read_csv("database_titanic.csv")
 
 # Muestra un título y una descripción en la aplicación Streamlit.
 st.write("""
-# Mi primera aplicación interactiva
+# LA primera aplicación interactiva
 ## Gráficos usando la base de datos del Titanic
 """)
 
